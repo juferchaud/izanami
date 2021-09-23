4586c5f3e97506f67e8669ae9ac7ee97ae53c841
pierrebrunin <pierre.brunin@maif.fr>
2021-09-23 11:26:52 +0200

    remove betaserie

--- example/example-spring/src/main/java/izanami/example/shows/ShowsApi.java
+++ example/example-spring/src/main/java/izanami/example/shows/ShowsApi.java
@@ -23,55 +23,22 @@ public class ShowsApi implements Shows {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ShowsApi.class);
 
-    private final FeatureClient featureClient;
-
     private final TvdbShowsApi tvdbShowsApi;
 
     private final BetaSerieApi betaSerieApi;
 
-public ShowsApi(FeatureClient featureClient, TvdbShowsApi tvdbShowsApi, BetaSerieApi betaSerieApi) {
-    this.featureClient = featureClient;
+public ShowsApi(TvdbShowsApi tvdbShowsApi, BetaSerieApi betaSerieApi) {
     this.tvdbShowsApi = tvdbShowsApi;
     this.betaSerieApi = betaSerieApi;
-    featureClient.onEvent("*", event -> {
-        Match(event).of(
-                Case($(instanceOf(FeatureEvent.FeatureCreated.class)), c -> {
-                    LOGGER.info("{} is created with enable = {}", c.feature().id(), c.feature().enabled());
-                    return Done.getInstance();
-                }),
-                Case($(instanceOf(FeatureEvent.FeatureUpdated.class)), c -> {
-                    LOGGER.info("{} is updated with enable = {}", c.feature().id(), c.feature().enabled());
-                    return Done.getInstance();
-                }),
-                Case($(instanceOf(FeatureEvent.FeatureDeleted.class)), c -> {
-                    LOGGER.info("{} is deleted", c.id());
-                    return Done.getInstance();
-                })
-        );
-    });
 }
 
     @Override
     public List<ShowResume> search(String serie) {
-        Features features = this.featureClient.features("mytvshows:providers:*").get();
-        if (features.isActive("mytvshows:providers:tvdb")) {
-            return tvdbShowsApi.search(serie);
-        } else if (features.isActive("mytvshows:providers:betaserie")) {
-            return betaSerieApi.search(serie);
-        } else {
-            return List.empty();
-        }
+        return tvdbShowsApi.search(serie);
     }
 
     @Override
     public Option<Show> get(String id) {
-        Features features = this.featureClient.features("mytvshows:providers:*").get();
-        if (features.isActive("mytvshows:providers:tvdb")) {
-            return tvdbShowsApi.get(id);
-        } else if (features.isActive("mytvshows:providers:betaserie")) {
-            return betaSerieApi.get(id);
-        } else {
-            return Option.none();
-        }
+        return tvdbShowsApi.get(id);
     }
 }
